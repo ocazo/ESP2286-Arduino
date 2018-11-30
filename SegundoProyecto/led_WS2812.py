625063ae6b307c5cfb948e169a13f46b1ca4e233
ocazo <ocazo@hotmail.com>
2018-11-30 11:35:40 -0500

    Create SegundoProyecto/led_WS2812.py

---
+++ SegundoProyecto/led_WS2812.py
@@ -0,0 +1,16 @@
+vuelta = 1
+while vuelta < 6:  
+  print("Vuelta " +str(vuelta))
+  vuelta = vuelta + 1
+  ledR.on()
+  time.sleep(0.5)
+  ledR.off()
+  time.sleep(0.5)
+  ledG.on()
+  time.sleep(0.5)
+  ledG.off()
+  time.sleep(0.5)
+  ledB.on()
+  time.sleep(0.5)
+  ledB.off()
+  time.sleep(0.5)
